42b7dc1135aff5a647f01db1ead7028258078890
Jonaben1 <jonaben7@gmail.com>
2022-06-04 16:17:21 +0100

    see

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- *  main - To print out lowercase letters
- *  Description - The main.c will exlude two letters
+ *  main - The function to print out single digit numbers
+ *  Description: The number must be less than 10
  *  Return: Always(0) Success
-*/
+ */
+
 int main(void)
 {
-	int x;
+	int i;
 
-	x = 0;
-	while (x < 10)
+	i = 0;
+	while (i < 10)
 	{
-		putchar(x);
-		x++;
+		putchar(i);
+		i++;
 	}
 	putchar('\n');
 	return (0);
 }
-
