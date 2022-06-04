d0a253d151476167be82a9d1dbb2d9b3de230228
Jonaben1 <jonaben7@gmail.com>
2022-06-04 16:03:35 +0100

    life

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - To print out lowercase letters
+ *  Description - The main.c will exlude two letters
+ *  Return: Always(0) Success
+*/
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
