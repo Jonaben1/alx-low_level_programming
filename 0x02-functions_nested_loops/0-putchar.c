f9b96a47e07c98f327f1af6312a2d4d27fa364ba
Jonathan Okah <jonaben7@gmail.com>
2022-06-07 22:48:30 +0100

    surprise

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main  - Entry point funct to print
+ *
+ * Return: On success 1.
+ *
+ */
+
+int main(void)
+{
+	int bum;
+	bum  = '0';
+	while (bum <= '9')
+	{
+		_putchar(bum);
+		bum++;
+	}
+	_putchar('\n');
+	return (0);
+}
