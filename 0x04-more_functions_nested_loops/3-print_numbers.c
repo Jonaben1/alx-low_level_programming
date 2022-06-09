9eeb96b085aa9822d3676d72df2b464667b3707a
Jonathan Okah <jonaben7@gmail.com>
2022-06-09 15:50:06 +0100

    smile

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,8 +9,8 @@ void print_numbers(void)
 	char x;
 
 	for (x = '0'; x <= '9'; x++)
-	{
+
 		_putchar(x);
-	}
+
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,6 +4,7 @@
  *  Return: Always 0 success
 */
 
+
 void print_most_numbers(void)
 {
 	int x;
@@ -11,9 +12,9 @@ void print_most_numbers(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		if (x != '2' && x != '4')
-		{
+
 			_putchar(x);
-		}
+
 	}
-	return (0);
+	_putchar('\n');
 }
