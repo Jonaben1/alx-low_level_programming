83946f3862610b1bbf86c2e0c4925320b66b690f
Jonathan Okah <jonaben7@gmail.com>
2022-06-11 19:57:30 +0100

    lifewire

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+   	int largest;
+   	if (a > b)
+	{
+		if (a > c)
+		{
+			if(b > c)
+			{
+				largest = a;
+			}
+			else
+			{
+				largest = a;
+			}
+		}
+		else
+		{
+			largest = c;
+		}
+	}
+	else if (b > c && a > c)
+	{
+		largest  = b;
+	}
+
+	else
+	{
+		largest  =  c;
+
+	}
+
+	return (largest);
+}
