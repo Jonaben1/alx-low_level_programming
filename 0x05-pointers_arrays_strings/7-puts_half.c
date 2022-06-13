0c527c090e2b046702dd229a89d2e2a3b0da9859
Jonathan Okah <jonaben7@gmail.com>
2022-06-13 23:31:45 +0100

    xup

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,17 @@ void puts_half(char *str)
 
 	for (ct = 0; str[ct] != '\0'; ct++)
 		;
-	if ((ct % 2) == 0)
 	{
-		for (even = ct / 2; str[even] != '\0'; even++)
-			_putchar(str[even]);
-	}
-	else
-	{
-		for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
+		if ((ct % 2) == 0)
+		{
+			for (even = ct / 2; str[even] != '\0'; even++)
+			{
+				_putchar(str[even]);
+			}
+		else
+		{
+			for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
 			_putchar(str[neg + 1]);
-	}
+		}
 	_putchar('\n');
 }
