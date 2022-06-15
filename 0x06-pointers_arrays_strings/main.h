a7f5397729da137783a184c1e445d1b8a78f31db
Jonathan Okah <jonaben7@gmail.com>
2022-06-15 21:55:23 +0100

    not compulsory

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+char *string_toupper(char *n);
+char *cap_string(char *n);
+void reverse_array(int *a, int n);
 int *_strncpy(char *dest, chart *src, int c);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
