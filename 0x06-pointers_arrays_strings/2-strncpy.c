da636b087c7c7f892894966395af2506794ea702
Jonathan Okah <jonaben7@gmail.com>
2022-06-16 10:39:06 +0100

    fortunately

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * *_strncpy - copies a string
- * @dest: destination
- * @src: source
- * @n: number of bytes
- * Return: Always 0.
+ * _strncpy - Entry point
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
+		while (i < n)
+			dest[i++] = '\0';
+
 	return (dest);
 }
