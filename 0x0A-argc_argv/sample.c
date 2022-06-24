4fc6b296074f2a125f1aa6ba656ddf663f171fe3
Jonaben1 <jonaben7@gmail.com>
2022-06-24 13:32:33 +0100

    The same issue has started again.

--- 0x0A-argc_argv/sample.c
+++ 0x0A-argc_argv/sample.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,11 +11,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
-	{
+	for (i = 0; i < argc, i++)
 		printf("%s\n", argv[i]);
-	}
-	return (0);
 }
 
 
