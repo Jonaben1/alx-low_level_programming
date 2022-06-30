781c9130c0f6910ff084980abd5e0b7625f41a36
Jonaben1 <jonaben7@gmail.com>
2022-06-30 14:38:03 +0100

    I am getting it slowly

--- 0x0B-malloc_free/0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0B-malloc_free/0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,10 +11,10 @@ void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
-	ptr = malloc(sizeof(int*) * b);
+	ptr = malloc(sizeof(int *) * b);
 	if (ptr == NULL)
 	{
-		exit (0);
+		exit(0);
 	}
 	return (ptr);
 }
