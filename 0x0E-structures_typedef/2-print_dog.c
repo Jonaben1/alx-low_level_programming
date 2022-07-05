e96e7a575827807ee284ba0aca69cf1440129766
Jonaben1 <jonaben7@gmail.com>
2022-07-05 15:40:53 +0100

    The Hard way is the easy way

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,18 +13,18 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d -> name == NULL)
+	if (d->name == NULL)
 	{
-		d -> name  = "(nil)";
+		d->name  = "(nil)";
 	}
-	if (d -> age <= 0)
+	if (d->age <= 0)
 		printf("Age: (nil)\n");
 
-	if (d -> owner == NULL)
-		d -> owner  = "(nill)";
+	if (d->owner == NULL)
+		d->owner  = "(nill)";
 
-	printf("Name: %s\n", d -> name);
-	printf("Age: %.6f\n", d -> age);
-	printf("Owner: %s\n", d -> owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
 
