bd04837d031df6121f5d4d4909564f1fe4a8c40c
Jonathan Okah <jonaben7@gmail.com>
2022-07-06 22:05:46 +0100

    Pressing forward to the goal

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
