582fb8d420c4c4e8c22addf78565aa3cb0914784
Jonathan Okah <jonaben7@gmail.com>
2022-07-06 16:02:11 +0100

    Printing names using pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints name
+ * @name: the name to be printed
+ * @f: a pointer function to be called
+ * Return: Always 0 success
+*/
+
+void print_name(char *name, void (*f)(char*))
+{
+	f(name);
+}
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_POINTERS_N_
+#define _FUNCTION_POINTERS_N_
+
+int _putchar(char c);
+void print_name(char *name, void(*f)(char *));
+#endif
