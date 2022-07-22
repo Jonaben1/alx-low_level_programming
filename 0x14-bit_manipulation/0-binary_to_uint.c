168045bc52c43a80fdbe0fc81106d39ed18ce643
Jonathan Okah <jonaben7@gmail.com>
2022-07-22 10:19:53 +0100

    Bit manipulation in C'

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - a function that converts a binary to an unsigned int
+ * @b: pointer used
+ * Return: Always 0 success
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i]-'0';
+		i++;
+	}
+	return (val);
+}
