0715362233d807f15c733067b201d3191ed8f5b6
Jonathan Okah <jonaben7@gmail.com>
2022-07-22 11:44:55 +0100

    Converting decimal to binary in C

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,14 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	printf("%lu",n & 1);
-}
+	unsigned int mask=32768;
 
+	while(mask > 0)
+	{
+		if((n & mask) == 0 )
+			_putchar('0');
+		else
+			_putchar('1');
+		mask = mask >> 1;
+	}
+}
