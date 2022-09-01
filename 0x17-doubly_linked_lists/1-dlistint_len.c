d1e8988f9ddee3cedbe0d9a6f31f7bab49c1f287
Jonathan Okah <jonaben7@gmail.com>
2022-09-01 20:52:33 +0100

    lenngth of int

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len  - a function that returns the>
+ * number of elements in a linked list
+ * @h: the node head
+ * return: number of node (success)
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+	while(h != NULL)
+	{
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
